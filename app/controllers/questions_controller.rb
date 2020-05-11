999c6a01142a9a8af861e349f5943b2945e66b7f
Ugochukwu Nwokoro <ugonwokoro@yahoo.com>
2020-05-11 14:51:10 +0100

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,20 @@
 class QuestionsController < ApplicationController
 
-  def answer
-  @user_input = params[:user_input]
-  @answer = 'Hi'
-
-
-
+  def ask
   end
+
+
+  def answer
+
+   @user_input = params[:user_input]
+
+  if @user_input.downcase == "i am going to work right now!"
+  @answer = "Great!"
+elsif @user_input.end_with?("?")
+  @answer = "Silly question, get dressed and go to work!"
+else
+  @answer = "I don't care, get dressed and go to work!"
+end
+  end
+
 end
